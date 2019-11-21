8fe0cd8c5f4c5c9833a2a281ce7303c7fd8e68a4
Arpine <arpine_grigoryan@edu.aua.am>
2019-11-21 22:58:47 +0400

    My final commit of this problem

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
 const util = require("./util.js");
-const matrix = require("./matrix.js");
 
 m1 = [ [1, 2],
        [3, 4],
        [5, 6] ];
 
 m2 = [ [1, 2, 3, 4],
-       [5, 6, 7, 8],
        [5, 6, 7, 8] ];
 
-//matrix.printMatrix(util.mutiplyMatrices(m1, m2));
-/*2*/util.mutiplyMatrices(m1, m2);
+util.printMatrix(util.mutiplyMatrices(m1, m2));
